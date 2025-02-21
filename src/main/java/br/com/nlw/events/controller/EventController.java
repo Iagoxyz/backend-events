8938e441bf7223b6958f26ddea3d63dd00696a38
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-02-21 11:22:07 -0300

    Controller and service layer

---
+++ src/main/java/br/com/nlw/events/controller/EventController.java
@@ -0,0 +1,22 @@
+package br.com.nlw.events.controller;
+
+import br.com.nlw.events.model.Event;
+import br.com.nlw.events.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+    @PostMapping("/events")
+    public Event addNewEvent(@RequestBody Event newEvent) {
+        return eventService.addNewEvent(newEvent);
+    }
+}
