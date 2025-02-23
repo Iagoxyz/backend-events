38ca15614088eb502b571cda187a11bd3479e3d7
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-02-23 18:34:14 -0300

    Many-to-one association

--- src/main/java/br/com/nlw/events/service/SubscriptionService.java
+++ src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -1,13 +1,38 @@
 package br.com.nlw.events.service;
 
+import br.com.nlw.events.model.Event;
 import br.com.nlw.events.model.Subscription;
 import br.com.nlw.events.model.User;
+import br.com.nlw.events.repository.EventRepository;
+import br.com.nlw.events.repository.SubscriptionRepository;
+import br.com.nlw.events.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SubscriptionService {
 
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private SubscriptionRepository subscriptionRepository;
+
     public Subscription createNewSubscription(String eventName, User user) {
+        // Recuperar o evento pelo nome
+
+        Event event = eventRepository.findByPrettyName(eventName);
+        user = userRepository.save(user);
+
+        Subscription subs = new Subscription();
+        subs.setEvent(event);
+        subs.setSubscriber(user);
+
+        Subscription result = subscriptionRepository.save(subs);
+        return result;
 
     }
 }
