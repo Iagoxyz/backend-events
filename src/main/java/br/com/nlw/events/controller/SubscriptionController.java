3dcbffb16949aad6240eccb73783c5cc6388bc6a
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-02-24 11:33:54 -0300

    Mapping service layer

--- src/main/java/br/com/nlw/events/controller/SubscriptionController.java
+++ src/main/java/br/com/nlw/events/controller/SubscriptionController.java
@@ -1,8 +1,10 @@
 package br.com.nlw.events.controller;
 
 import br.com.nlw.events.dto.ErrorMessage;
-import br.com.nlw.events.dto.SubscriptionConflictException;
+import br.com.nlw.events.dto.SubscriptionResponse;
+import br.com.nlw.events.exception.SubscriptionConflictException;
 import br.com.nlw.events.exception.EventNotFoundException;
+import br.com.nlw.events.exception.UserIndicadorNotFoundException;
 import br.com.nlw.events.model.Subscription;
 import br.com.nlw.events.model.User;
 import br.com.nlw.events.service.SubscriptionService;
@@ -19,10 +21,10 @@ public class SubscriptionController {
     @Autowired
     private SubscriptionService service;
 
-    @PostMapping("/subscription/{prettyName}")
-    public ResponseEntity<?> createSubscription(@PathVariable String prettyName, @RequestBody User subscriber) {
+    @PostMapping({"/subscription/{prettyName}", "/subscription/{prettyName}/{userId}"})
+    public ResponseEntity<?> createSubscription(@PathVariable String prettyName, @RequestBody User subscriber, @PathVariable(required = false) Integer userId) {
         try {
-            Subscription result = service.createNewSubscription(prettyName, subscriber);
+            SubscriptionResponse result = service.createNewSubscription(prettyName, subscriber, userId);
             if (result != null) {
                 return ResponseEntity.ok().body(result);
             }
@@ -30,6 +32,8 @@ public class SubscriptionController {
         } catch (EventNotFoundException e) {
             return ResponseEntity.status(404).body(new ErrorMessage(e.getMessage()));
         } catch (SubscriptionConflictException e) {
+            return ResponseEntity.status(404).body(new ErrorMessage(e.getMessage()));
+        } catch (UserIndicadorNotFoundException e) {
             return ResponseEntity.status(404).body(new ErrorMessage(e.getMessage()));
         }
         return ResponseEntity.badRequest().build();
--- src/main/java/br/com/nlw/events/service/SubscriptionService.java
+++ src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -1,7 +1,9 @@
 package br.com.nlw.events.service;
 
-import br.com.nlw.events.dto.SubscriptionConflictException;
+import br.com.nlw.events.dto.SubscriptionResponse;
+import br.com.nlw.events.exception.SubscriptionConflictException;
 import br.com.nlw.events.exception.EventNotFoundException;
+import br.com.nlw.events.exception.UserIndicadorNotFoundException;
 import br.com.nlw.events.model.Event;
 import br.com.nlw.events.model.Subscription;
 import br.com.nlw.events.model.User;
@@ -23,7 +25,7 @@ public class SubscriptionService {
     @Autowired
     private SubscriptionRepository subscriptionRepository;
 
-    public Subscription createNewSubscription(String eventName, User user) {
+    public SubscriptionResponse createNewSubscription(String eventName, User user, Integer userId) {
         // Recuperar o evento pelo nome
 
         Event event = eventRepository.findByPrettyName(eventName);
@@ -35,9 +37,18 @@ public class SubscriptionService {
             userRec = userRepository.save(user);
         }
 
+        User indicador = null;
+        if (userId != null) {
+            indicador = userRepository.findById(userId).orElse(null);
+            if (indicador == null) {
+                throw new UserIndicadorNotFoundException("Usuário " + userId + "indicador não existe!");
+            }
+        }
+
         Subscription subs = new Subscription();
         subs.setEvent(event);
         subs.setSubscriber(userRec);
+        subs.setIndication(indicador);
 
         Subscription sub = subscriptionRepository.findByEventAndSubscriber(event, userRec);
         if (sub != null) {
@@ -45,6 +56,6 @@ public class SubscriptionService {
         }
 
         Subscription result = subscriptionRepository.save(subs);
-        return result;
+        return new SubscriptionResponse(result.getSubscriptionNumber(), "http://codecraft.com/subscription/" + result.getEvent().getPrettyName() + "/" + result.getSubscriber().getId());
     }
 }
